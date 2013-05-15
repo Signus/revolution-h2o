df263b1fdc098e158e1198f7bfbe511fd1ab2330
ccard <ccard@mymail.mines.edu>
2013-05-15 11:24:10 -0600

    added method to allow the toggling of the mute button

--- CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/GameLauncher.java
+++ CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/GameLauncher.java
@@ -9,9 +9,12 @@ package csci307.theGivingChild.CleanWaterGame;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class GameLauncher extends Activity {
 
+    public static boolean MUTE_SOUND_EFX=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,19 @@ public class GameLauncher extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.game_launcher, menu);
         return true;
+    }
+
+
+    /**
+    * This function is linked to the mute button in the activity_game_launcher.xml file
+    * @param The view that is calling this method
+    */
+    public void toggleMute(View v)
+    {
+        ImageButton im = (ImageButton)v;
+
+        im.setImageResource((MUTE_SOUND_EFX ? R.drawable.unmuted : R.drawable.mute));
+        MUTE_SOUND_EFX = (MUTE_SOUND_EFX ? false : true);
     }
     
 }
