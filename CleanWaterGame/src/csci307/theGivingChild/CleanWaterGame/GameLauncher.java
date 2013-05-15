7ec93e255c7d32e5a3c5848c8db0e19521e08add
ccard <ccard@mymail.mines.edu>
2013-05-15 12:30:06 -0600

    added methods to make the buttons in the layout functional

--- CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/GameLauncher.java
+++ CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/GameLauncher.java
@@ -6,8 +6,10 @@
  
 package csci307.theGivingChild.CleanWaterGame;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
@@ -15,6 +17,8 @@ import android.widget.ImageButton;
 public class GameLauncher extends Activity {
 
     public static boolean MUTE_SOUND_EFX=false;
+    private final String gvingChildUrl = "http://www.thegivingchild.org/home/DONATE.html";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +44,38 @@ public class GameLauncher extends Activity {
 
         im.setImageResource((MUTE_SOUND_EFX ? R.drawable.unmuted : R.drawable.mute));
         MUTE_SOUND_EFX = (MUTE_SOUND_EFX ? false : true);
+    }
+
+    /**
+    * This method is linked to the donate button in the activity
+    * it opens the donation url in their phones web browser
+    * @param the view calling this method
+    */
+    public void openDonate(View v)
+    {
+        Intent donation = new Intent(Intent.ACTION_VIEW, Uri.parse(gvingChildUrl));
+        startActivity(donation);
+    }
+
+    /**
+    * This method opens the minigames selection activity and is linked
+    * to the play game button
+    * @param the view calling this mehtod
+    */
+    public void openMiniGames(View v)
+    {
+        Intent play = new Intent(this, MiniGame_selection.class);
+        startActivity(play);
+    }
+
+    /**
+    * This method opens the extras menu activity and is linked to the extras button
+    * @param the view calling this method
+    */
+    public void openExtras(View v)
+    {
+        Intent extra = new Intent(this, Extras_menu.class);
+        startActivity(extra);
     }
     
 }
