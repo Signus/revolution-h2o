659872607ebcedad83c6b7afed30d4b4180d835f
ccard <ccard@mymail.mines.edu>
2013-05-16 11:07:58 -0600

    renamed error dialog to bugreport and added code

---
+++ CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/BugReport.java
@@ -0,0 +1,46 @@
+/**
+* Authors: Maria, Chris, Tony, Gurpreet, Dylan, Dustin
+* Date: 5/16/13
+* Verson: 1.0
+* Descripion: this activity will show up as a dilog and allow for error reporting
+*
+* Note: depending on the intent (system reprot v user report) youll pass in a boolean
+*   true for user report and false for system report
+*/
+package csci307.theGivingChild.CleanWaterGame;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class BugReport extends Activity {
+
+	public static final Stirng ERROR_CODE_KEY = "csci370.theGivingChild.Error.boolerror";
+	private boolean userSysBug;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		userSysBug = false;
+		if(savedInstanceState != null)
+		{
+			if (savedInstanceState.getBoolean(ERROR_CODE_KEY,false)) 
+			{
+				userSysBug = true;
+				setContentView(R.layout.userbugreport);
+			}
+			else
+			{
+				setContentView(R.layout.systembugreport);
+			}
+			
+		}
+		else
+		{
+			//error fun
+		}
+
+		
+	}
+
+}
