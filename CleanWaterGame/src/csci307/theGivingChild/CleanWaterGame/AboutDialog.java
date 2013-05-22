b32218a5cbe12b326d5f1c1f97a75f9a897933fe
ccard <ccard@mymail.mines.edu>
2013-05-22 11:31:34 -0600

    added code and layout files for about dialog and to the manifest to make it work

---
+++ CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/AboutDialog.java
@@ -0,0 +1,27 @@
+/**
+ * Authors: Chris, Tony, Gurpreet, Maria, Dustin,Dylan
+ * Date: 5/22/13
+ * Version:1.0
+ * Description: This is a dialog containing info about the app and the giving child
+ */
+package csci307.theGivingChild.CleanWaterGame;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+public class AboutDialog extends Activity {
+	
+	@Override
+	public void onCreate(Bundle onSaveInstance)
+	{
+		super.onCreate(onSaveInstance);
+		setContentView(R.layout.about_display);
+		
+		TextView t = (TextView)findViewById(R.id.aboutDisplay);
+		t.setMovementMethod(new ScrollingMovementMethod());
+	}
+
+}
