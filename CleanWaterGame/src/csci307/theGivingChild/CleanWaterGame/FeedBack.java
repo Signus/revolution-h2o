12631f77ef584f1525664634f8896ae7f3b9af1c
ccard <ccard@mymail.mines.edu>
2013-06-11 13:05:38 -0600

    added comments and removed unnecessary code

--- CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/FeedBack.java
+++ CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/FeedBack.java
@@ -1,12 +1,11 @@
 /**
 * Authors: Chris Card, Dylan Chau, Maria Deslis, Dustin Liang, Gurpreet Nanda, Tony Nguyen
 * Date: 5/16/13
-* Verson: 1.0
-* Descripion: this activity will show up as a dialog and allows the user to send feedback via e-mail
+* Version: 1.0
+* Description: this activity will show up as a dialog and allows the user to send feedback via e-mail
 *
-* Note: depending on the intent (system report v user report) you'll pass in a boolean
-*   true for user report and no boolean for system
-*   for system error you'll pass in the code and description to the intent
+* History:
+*  5/16/13 original 1.0
 */
 package csci307.theGivingChild.CleanWaterGame;
 
@@ -25,52 +24,42 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class FeedBack extends Activity {
-
-	public static final String ERROR_CODE_KEY = "csci370.theGivingChild.Error.boolerror";
-	public static final String ERROR_CODE_NUM = "csci370.theGivingChild.Error.code";
-	public static final String ERROR_CODE_DESC = "csci370.theGivingChild.Error.desc";
-
+	
 	private Spinner related;
 	private Button send;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Bundle b = this.getIntent().getExtras();
+	
+			
+		setContentView(R.layout.userbugreport);
 		
-		if(b != null)
-		{
-			if (b.getBoolean(ERROR_CODE_KEY,false)) 
-			{
-				setContentView(R.layout.userbugreport);
-				related = (Spinner)findViewById(R.id.userbug_related_list);
-				ArrayAdapter<CharSequence> adapt = ArrayAdapter.createFromResource(this,
-									R.array.userbug_relatedto_spinner,android.R.layout.simple_spinner_item);
-				adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-				related.setAdapter(adapt);
-				send = (Button)findViewById(R.id.userbug_send);
-				send.setOnClickListener(new OnClickListener(){
+		//This populates the relates spinner and allows to get the users choice
+		related = (Spinner)findViewById(R.id.userbug_related_list);
+		ArrayAdapter<CharSequence> adapt = ArrayAdapter.createFromResource(this,
+							R.array.userbug_relatedto_spinner,android.R.layout.simple_spinner_item);
+		adapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		related.setAdapter(adapt);
+		
+		send = (Button)findViewById(R.id.userbug_send);
+		send.setOnClickListener(new OnClickListener(){
 
-					@Override
-					public void onClick(View v) {
-						String subject = related.getSelectedItem().toString()+":";
-						Intent gmail = new Intent(Intent.ACTION_VIEW);
-						gmail.setClassName("com.google.android.gm","com.google.android.gm.ComposeActivityGmail");
-						gmail.putExtra(Intent.EXTRA_EMAIL, new String[] {"thegivingchild@gmail.com"});//set this to the feed back email site
-						gmail.setData(Uri.parse("thegivingchild@gmail.com"));
-						gmail.putExtra(Intent.EXTRA_SUBJECT, subject);
-						gmail.setType("plain/text");
-						startActivity(gmail);
-						finish();
-					}
-					
-				});
+			@Override
+			public void onClick(View v) {
+				String subject = related.getSelectedItem().toString()+":";
+				//This sets up an email and uses the user email, the to line and the subject
+				//to facilitate a easier feed back experience when they hit the compose button
+				Intent gmail = new Intent(Intent.ACTION_VIEW);
+				gmail.setClassName("com.google.android.gm","com.google.android.gm.ComposeActivityGmail");
+				gmail.putExtra(Intent.EXTRA_EMAIL, new String[] {"thegivingchild@gmail.com"});//set this to the feed back email site
+				gmail.setData(Uri.parse("thegivingchild@gmail.com"));
+				gmail.putExtra(Intent.EXTRA_SUBJECT, subject);
+				gmail.setType("plain/text");
+				startActivity(gmail);
+				finish();
 			}
 			
-		}
-		else
-		{
-			//error fun
-		}
+		});
 	}
 }
