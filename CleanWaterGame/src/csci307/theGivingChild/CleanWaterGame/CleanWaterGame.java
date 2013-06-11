9936f91559b57b4da8cd1a7504e1723cde1e4533
ccard <ccard@mymail.mines.edu>
2013-06-11 13:03:47 -0600

    added and updated comments

--- CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/CleanWaterGame.java
+++ CleanWaterGame/src/csci307/theGivingChild/CleanWaterGame/CleanWaterGame.java
@@ -2,10 +2,11 @@
 * Authors: Chris Card, Dylan Chau, Maria Deslis, Dustin Liang, Gurpreet Nanda, Tony Nguyen
 * Date: 5/31/13
 * Version: 1.0
-* Description: This is the application class that is used for context and bug reporting
+* Description: This is the application class that is used for context and bug reporting and is a
+*              singleton patter to facilitate getting the context and playing music 
 *
 * History:
-*    5/31/13 original
+*    5/31/13 original 1.0
 */
 package csci307.theGivingChild.CleanWaterGame;
 
@@ -15,9 +16,11 @@ import android.media.MediaPlayer;
 import org.acra.*;
 import org.acra.annotation.*;
 
+//This is the acra code for the bug reporting it defines what will happen when a exception occurs
+//and what is sent
 @ReportsCrashes(
     formKey="",//depreciated not used but still required
-    mailTo="ccard@mymail.mines.edu",
+    mailTo="thegivingchild@gmail.com",
     customReportContent={ReportField.APP_VERSION_CODE, ReportField.ANDROID_VERSION, ReportField.STACK_TRACE},
     mode=ReportingInteractionMode.TOAST,
     resToastText=40
@@ -26,11 +29,14 @@ public class CleanWaterGame extends Application {
 	private static CleanWaterGame instance;
 	 private MediaPlayer menuMusic;
 	 private MediaPlayer selectSound;
+	 
+	 
 	@Override
 	public void onCreate()
 	{
 		super.onCreate();
 		instance = this;
+		//This initializes the bug reporting
 		ACRA.init(this);
 		
 		 //Set menu music source
@@ -50,23 +56,36 @@ public class CleanWaterGame extends Application {
 		}     
 	}
 	
-	
-	
+	/**
+	 * This gets the current instance of CleanWaterGame
+	 * @return an instance of this class
+	 */
 	public static CleanWaterGame getInstance()
 	{
 		return instance;
 	}
 	
+	/**
+	 * This method pauses the menu music
+	 */
 	public void pauseMenuMusic()
 	{
+		//If this check is not done it results in an error
+		//the mediaplayer can only be paused when it is playing music
 		if(menuMusic.isPlaying())menuMusic.pause();
 	}
 	
+	/**
+	 * starts the looped menu music media player
+	 */
 	public void playMenuMusic()
 	{
 		menuMusic.start();
 	}
 	
+	/**
+	 * Starts the button sound media player 
+	 */
 	public void playBtnSound()
 	{
 		selectSound.start();
